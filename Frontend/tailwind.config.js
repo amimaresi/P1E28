9486456cd52cf423607b37139fd2606e188952be
amimaresi <ma_hassam@esi.dz>
2024-03-22 15:59:35 +0100

    setup version

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -5,7 +5,19 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: '#EFF3FF',
+        buttonDark: '#190482',
+        buttonLight: '#4733E4',
+        textDark: '#020B1F',
+        textLight: '#EFF3FF',
+      },
+      fontFamily: {
+        title: ['inter', 'sans-serif'],
+        text: ['open-sans', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
