575275240b30a8efcb1825dd13d24f387a38c908
rawane0310 <118125151+rawane0310@users.noreply.github.com>
2024-03-25 00:52:30 +0100

    Update ConfJournal.js

--- backend/schema/ConfJournal.js
+++ backend/schema/ConfJournal.js
@@ -2,20 +2,23 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const ConfJournSchema = new Schema ({
-acronyme: {
+_id: {  //acronyme
     type: String,
     required: true
 },
-classement: {
+nom: {
     type: String,
     required: true
 },
-publications: [
-    {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "publication"
-    }
-]
+periodicite: {
+    type: String,
+    
+},
+lien: {
+    type: String,
+    
+}
+
 }, { timestamps: true });
 
 const ConfJournal = mongoose.model("confJournal",ConfJournSchema);
