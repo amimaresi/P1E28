24148172cc6e8064d4a4bcc96f087f604c58a685
rawane0310 <118125151+rawane0310@users.noreply.github.com>
2024-04-01 23:02:58 +0100

    Update majM.js

--- backend/màj/majM.js
+++ backend/màj/majM.js
@@ -1,35 +1,28 @@
-const { MongoClient } = require('mongodb');
+const mongoose = require('mongoose');
+const Chercheur = require('../../schema/Chercheur');  
+const Publication = require('../../schema/Publication'); 
 
 
 const majManuelle = async () => { 
     try {
-        // Connexion à la base de données
-        const client = await MongoClient.connect('lien de bdd');
-       // console.log("Connexion à MongoDB établie avec succès");
+        
 
-        // Sélectionner la base de données
-        const db = client.db('test');
+        // Récupérer tous les documents
+        const docs = await Chercheur.find({}).exec();
 
-        // Collection dans laquelle extraire les données
-        const collection = db.collection('chercheurs');
-        const collection1 = db.collection('publications');
-
-        // Récupérer tous les chercheures
-        const docs = await collection.find({}).toArray();
-
-        // Parcourir les chercheures pour extraire les données
+        // Parcourir les documents pour extraire les données
         for (let doc of docs) {
             const cherch = doc.nom_complet;
             const lien = "https://dblp.org/search/publ/api?q=" + cherch + "&format=json";
             const response = await fetch(lien);
             const data = await response.json();
-            const tabPublie = data.result.hits.hit;  //toutes le publications recupérées de DBLP
+            const tabPublie = data.result.hits.hit;
             let donneesAInserer = [];
 
             for (let i = 0; i < tabPublie.length; i++) {
                 let tabA = [];
                 const auteurs = tabPublie[i].info.authors.author;
-                let rang = -1; //Si c pas le meme nom de chercheur
+                let rang = -1;
                 for (let j = 0; j < auteurs.length; j++) {
                     tabA[j] = auteurs[j].text;
                     if (tabA[j] === cherch) {
@@ -55,31 +48,30 @@ const majManuelle = async () => {
                 }
             }
 
-            const t = donneesAInserer.length; // le nombre de publication dans DBLP apres tries
-            const v = doc.publications ? doc.publications.length : 0;  //nombre de publication chez le chercheur
-            const s = t - v;  //la différence
+            const t = donneesAInserer.length;
+            const v = doc.publications ? doc.publications.length : 0;
+            const s = t - v;
 
-            if (s > 0 && v!== 0) { 
+            if (s > 0 && v !== 0) {
                 let ins = [];
                 for (let z = 0; z < s; z++) {
                     ins[z] = donneesAInserer[z];
-                    await collection.updateOne( //ajouter le idPub dans le champ du chercheur
+                    await Chercheur.updateOne(
                         { _id: doc._id },
                         { $push: { publications: donneesAInserer[z]._id } }
                     );
                 }
-                const result = await collection1.insertMany(ins); //ajouter dans la collection Publications
-                //console.log('Données insérées avec succès');
+                const result = await Publication.insertMany(ins);
+                console.log('Données insérées avec succès');
             }
         }
 
-        //console.log('Données traitées');
+        console.log('Données traitées');
 
-        // Fermer la connexion
-        client.close();
-        //console.log('Traitement terminé avec succès');
+       
+        console.log('Traitement terminé avec succès');
     } catch (error) {
-        //console.error('Une erreur s\'est produite :', error);
+        console.error('Une erreur s\'est produite :', error);
     }
 };
 
