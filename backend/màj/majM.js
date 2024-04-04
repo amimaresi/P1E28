9ee42d52207a2e0ce67eed3cf0ed1e95472d26a0
rawane0310 <118125151+rawane0310@users.noreply.github.com>
2024-04-04 21:24:29 +0100

    Update majM.js

--- backend/màj/majM.js
+++ backend/màj/majM.js
@@ -1,7 +1,6 @@
-
 const Chercheur = require('../../schema/Chercheur');  
 const Publication = require('../../schema/Publication'); 
-
+const conf = require('../../schema/ConfJournal');
 
 const majManuelle = async () => { 
     try {
@@ -12,54 +11,68 @@ const majManuelle = async () => {
 
         // Parcourir les documents pour extraire les données
         for (let doc of docs) {
-            const cherch = doc.nom_complet;
+            const cherch = doc.nomComplet;
+            
             const lien = "https://dblp.org/search/publ/api?q=" + cherch + "&format=json";
             const response = await fetch(lien);
             const data = await response.json();
             const tabPublie = data.result.hits.hit;
             let donneesAInserer = [];
 
-            for (let i = 0; i < tabPublie.length; i++) {
+
+            for (let i = 0; i < tabPublie.length ; i++) { 
+               
                 let tabA = [];
                 const auteurs = tabPublie[i].info.authors.author;
                 let rang = -1;
-                for (let j = 0; j < auteurs.length; j++) {
+                for (let j = 0; j < auteurs.length; j++) { //la liste des membres
                     tabA[j] = auteurs[j].text;
+                   
                     if (tabA[j] === cherch) {
                         rang = j + 1;
                     }
                 }
 
-                if (typeof tabPublie[i].info.volume !== "undefined" && typeof tabPublie[i].info.pages !== "undefined" && rang !== -1) {
+if (typeof tabPublie[i].info.volume === "undefined") { //si le volume est indefini
+   
+    tabPublie[i].info.volume = "indefini";
+
+}; 
+if (typeof tabPublie[i].info.pages === "undefined") { //si le nombre de page est indefini
+    tabPublie[i].info.pages = "indefini";
+};
+                if (rang !== -1) {
                     const publie = {
-                        _id: {
+                        
                             Date: tabPublie[i].info.year,
                             idCherch: doc._id,
-                            volume: tabPublie[i].info.volume,
+                            volume: tabPublie[i].info.volume ,
                             confJourn: tabPublie[i].info.venue,
-                            pages: tabPublie[i].info.pages,
-                            rang: rang
-                        },
+                            pages: tabPublie[i].info.pages ,
+                            rang: rang,
+                        
                         Titre: tabPublie[i].info.title,
                         Lien: tabPublie[i].info.ee,
                         Membres: tabA,
                     };
                     donneesAInserer.push(publie);
+                    const confjourn = await conf.findOneAndUpdate(
+                        { _id: tabPublie[i].info.venue }, 
+                        { _id: tabPublie[i].info.venue , type: tabPublie[i].info.type }, 
+                        { upsert: true, new: true } 
+                    );
                 }
             }
 
-            const t = donneesAInserer.length;
-            const v = doc.publications ? doc.publications.length : 0;
-            const s = t - v;
+            const t = donneesAInserer.length; //nombre de publication dans DBLP
+            const v = await Publication.countDocuments({ 'idCherch':  doc._id}); //nombre de publication d'un chercheur dans la bdd
+            const s = t - v; 
 
-            if (s > 0 && v !== 0) {
+            if (s > 0 ) {
                 let ins = [];
                 for (let z = 0; z < s; z++) {
                     ins[z] = donneesAInserer[z];
-                    await Chercheur.updateOne(
-                        { _id: doc._id },
-                        { $push: { publications: donneesAInserer[z]._id } }
-                    );
+                   
                 }
                 const result = await Publication.insertMany(ins);
                 console.log('Données insérées avec succès');
