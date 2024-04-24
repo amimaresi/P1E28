820bed75af93a5213ba6c9bfa58d987c73f2b1f7
beskie04 <mb_bachferrag@esi.dz>
2024-04-24 21:51:25 +0100

    forms added

--- Frontend/src/Pages/Technical Pages/Control Pannel/Pages/AddChercheur.jsx
+++ Frontend/src/Pages/Technical Pages/Control Pannel/Pages/AddChercheur.jsx
@@ -22,44 +22,416 @@ import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
 export default function Filtres() {
   const schema = yup.object().shape({
-    chercheur: yup.string().required(),
+    nom: yup.string().required("le nom est requis"),
+    prenom: yup.string().required("le prenom est requis"),
+   email: yup.string().email("email format is not valid").required(" l'email est requis"),
+    diplome: yup.string().required("le diplome est requis"),
+    etablissementdorigine: yup.string().required(" l'etabliisement d'origine est requis "),
+    gradeenseignement : yup.string().required(" le  grade-enseignement est requis "),
+    graderecherche : yup.string().required(" le grade-recherche est requis "),
+    qualitee : yup.string().required(" la qualitee est requise "),
+    hIndex : yup.string(),
+    equipe: yup.string(),
+    lien: yup.string(),
+    projet : yup.string(),
   });
 
   const form = useForm({
+    defaultValues: {
+      nom: "",
+      prenom: "",
+   email:"",
+    diplome:"",
+    etablissementdorigine: "",
+    gradeenseignement : "professeur",
+    graderecherche :"directeur de recherche" ,
+    qualitee :"enseignant-chercheur",
+    hIndex : "",
+    equipe: "",
+    lien:"",
+    projet :"",
+
+    },
     resolver: yupResolver(schema),
+
   });
 
   const onSubmit = (data) => {
     console.log('Filtres : ', data);
   };
-
+ 
+  
+  
   return (
     <>
       <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <div className='p-5 text-center '>
+            <h1 className='mb-4 text-xl font-bold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-2xl dark:text-white'>Ajouter un nouveau chercheur</h1>
+            </div>
+            <h2 className='text-1xl font-bold dark:text-white text-center'>les informations personnelles</h2>
+            <hr className='h-px my-8 bg-black  bg-opacity-50 border-0 '></hr>
           <FormField
             control={form.control}
-            name="chercheur"
+            name="nom"
             render={({ field }) => (
               <>
+              
                 <FormItem>
-                  <FormLabel>chercheur</FormLabel>
+                <div className='flex flex-row items-center justify-start  pt-5'>
+                  <div className='pl-5 pr-9 '>
+                  <FormLabel>Nom :</FormLabel>
+                  </div>
                   <FormControl>
-                    <Input
-                      placeholder="entrez le nom du chercheur"
+                  
+                    <Input className=' rounded-full w-300 h-7'
+                     
+                      placeholder="entrez le nom "
                       {...field}
+                      
+                      
                     />
+                    
                   </FormControl>
-                  <FormDescription>
-                    This is your public display name.
-                  </FormDescription>
+                  
+                  </div>
+                  <FormMessage />
+                </FormItem> 
+              </>
+            )}
+          />
+          
+      <FormField
+            control={form.control}
+            name="prenom"
+            render={({ field }) => (
+
+              <> 
+              <FormItem>
+                <div className='flex flex-row items-center justify-start   '>
+                  <div className='px-5'>
+                  <FormLabel >prénom :</FormLabel>
+                  </div>
+                  <FormControl>
+                  
+                    <Input className=' rounded-full w-300 h-7'
+                     
+                      placeholder="entrez le prénom "
+                      {...field}
+                      
+                    />
+                    
+                  </FormControl>
+                  </div>
                   <FormMessage />
                 </FormItem>
               </>
             )}
+            />
+            
+            <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <>
+              
+                <FormItem>
+                <div className='flex flex-row items-center justify-start  '>
+                  <div className='pl-5 pr-9 '>
+                  <FormLabel>Email :</FormLabel>
+                  </div>
+                  <FormControl>
+                  
+                    <Input className=' rounded-full w-300 h-7'
+                     
+                      placeholder="entrez l'email "
+                      {...field}
+                      
+                    />
+                    
+                  </FormControl>
+                  </div>
+                  <FormMessage />
+                </FormItem> 
+              </>
+            )}
           />
-          <Button type="submit">Submit</Button>
-        </form>
+          
+             
+            
+            <h2 className='text-1xl font-bold dark:text-white text-center'>les informations professionnelles</h2>
+            <hr className='h-px my-8 bg-black  bg-opacity-50 border-0 '></hr>
+            <FormField
+            control={form.control}
+            name="gradeenseignement"
+            render={({ field }) => (
+
+              <> 
+              <FormItem>
+                <div className='flex flex-row items-center justify-start   '>
+                  <div className='px-5'>
+                  <FormLabel >grade enseignement :</FormLabel>
+                  </div>
+                  <FormControl>
+                <Select onValueChange={field.onChange}
+                value={field.value} >
+                <div className='pl-6'>
+                 <SelectTrigger className="w-[180px] rounded-full pl-6 h-7">
+                 <SelectValue placeholder="choisissez" />
+                 </SelectTrigger>
+                 <SelectContent>
+                        <SelectItem value="professeur">Professeur</SelectItem>
+                        <SelectItem value="mca">MCA</SelectItem>
+                        <SelectItem value="mcb">MCB</SelectItem>
+                        <SelectItem value="maa">MAA</SelectItem>
+                        <SelectItem value="mab">MAB</SelectItem>
+                        <SelectItem value="null">NULL</SelectItem>
+                </SelectContent>
+                </div>
+                </Select>
+                </FormControl>
+                  </div>
+                  <FormMessage />
+                </FormItem>
+              </>
+            )}
+            />
+            <FormField
+            control={form.control}
+            name="graderecherche"
+            render={({ field }) => (
+
+              <> 
+              <FormItem>
+                <div className='flex flex-row items-center justify-start   '>
+                  <div className='px-5'>
+                  <FormLabel >grade recherche :</FormLabel>
+                  </div>
+                  <FormControl>
+                <Select onValueChange={field.onChange}
+                value={field.value}>
+                <div className='pl-6'>
+                 <SelectTrigger className="w-[180px] rounded-full pl-6 h-7">
+                 <SelectValue placeholder="choisissez" />
+                 </SelectTrigger>
+                 <SelectContent>
+                 <SelectItem value="directeur de recherche">Directeur de recherche</SelectItem>
+                        <SelectItem value="maitre de recherche">Maitre de recherche</SelectItem>
+                        <SelectItem value="charge de recherche">Charge de recherche</SelectItem>
+                        <SelectItem value="attache de recherche">Attache de recherche</SelectItem>
+                        <SelectItem value="null">NULL</SelectItem>
+                </SelectContent>
+                </div>
+                </Select>
+                </FormControl>
+                  </div>
+                  <FormMessage />
+                </FormItem> 
+              </>
+            )}
+            />
+            <FormField
+            control={form.control}
+            name="diplome"
+            render={({ field }) => (
+
+              <> 
+              <FormItem>
+                <div className='flex flex-row items-center justify-start   '>
+                  <div className='px-5'>
+                  <FormLabel >diplome :</FormLabel>
+                  </div>
+                  <FormControl>
+                  
+                    <Input className=' rounded-full w-300 h-7'
+                     
+                      placeholder=" "
+                      {...field}
+                      
+                    />
+                    
+                  </FormControl>
+                  </div>
+                  <FormMessage />
+                </FormItem>
+              </>
+            )}
+            />
+            
+            <FormField
+            control={form.control}
+            name="etablissementdorigine"
+            render={({ field }) => (
+
+              <> 
+              <FormItem>
+                <div className='flex flex-row items-center justify-start   '>
+                  <div className='px-5'>
+                  <FormLabel >etablissement d origine :</FormLabel>
+                  </div>
+                  <FormControl>
+                  
+                    <Input className=' rounded-full w-300 h-7'
+                     
+                      placeholder=" "
+                      {...field}
+                      
+                    />
+                    
+                  </FormControl>
+                  </div>
+                  <FormMessage />
+                </FormItem>
+              </>
+            )}
+            />
+            
+            <FormField
+            control={form.control}
+            name="qualitee"
+            render={({ field }) => (
+
+              <> 
+              <FormItem>
+                <div className='flex flex-row items-center justify-start   '>
+                  <div className='px-5'>
+                  <FormLabel >qualitee:</FormLabel>
+                  </div>
+                  <FormControl>
+                <Select onValueChange={field.onChange}
+                value={field.value} >
+                <div className='pl-6'>
+                 <SelectTrigger className="w-[180px] rounded-full pl-6 h-7">
+                 <SelectValue placeholder="choisissez" />
+                 </SelectTrigger>
+                 <SelectContent>
+                        <SelectItem value="enseignant-chercheur">Enseignant-chercheur</SelectItem>
+                        <SelectItem value="chercheur">Chercheur</SelectItem>
+                        <SelectItem value="doctorant">Doctorant</SelectItem>
+                </SelectContent>
+                </div>
+                </Select>
+                </FormControl>
+                  </div>
+                  <FormMessage />
+                </FormItem>
+              </>
+            )}
+            />
+            <FormField
+            control={form.control}
+            name="hIndex"
+            render={({ field }) => (
+
+              <> 
+              <FormItem>
+                <div className='flex flex-row items-center justify-start   '>
+                  <div className='px-5'>
+                  <FormLabel >h-index  :</FormLabel>
+                  </div>
+                  <FormControl>
+                  
+                    <Input className=' rounded-full w-300 h-7'
+                     
+                      placeholder=" "
+                      {...field}
+                      
+                    />
+                    
+                  </FormControl>
+                  </div>
+                  <FormMessage />
+                </FormItem>
+              </>
+            )}
+            />
+            <FormField
+            control={form.control}
+            name="equipe"
+            render={({ field }) => (
+
+              <> 
+              <FormItem>
+                <div className='flex flex-row items-center justify-start   '>
+                  <div className='px-5'>
+                  <FormLabel >equipe :</FormLabel>
+                  </div>
+                  <FormControl>
+                  
+                    <Input className=' rounded-full w-300 h-7'
+                     
+                      placeholder=" "
+                      {...field}
+                      
+                    />
+                    
+                  </FormControl>
+                  </div>
+                  <FormMessage />
+                </FormItem>
+              </>
+            )}
+            />
+            <FormField
+            control={form.control}
+            name="lien"
+            render={({ field }) => (
+
+              <> 
+              <FormItem>
+                <div className='flex flex-row items-center justify-start   '>
+                  <div className='px-5'>
+                  <FormLabel >lien :</FormLabel>
+                  </div>
+                  <FormControl>
+                  
+                    <Input className=' rounded-full w-300 h-7'
+                     
+                      placeholder=" "
+                      {...field}
+                      
+                    />
+                    
+                  </FormControl>
+                  </div>
+                  <FormMessage />
+                </FormItem>
+              </>
+            )}
+            />
+            <FormField
+            control={form.control}
+            name="projet"
+            render={({ field }) => (
+
+              <> 
+              <FormItem>
+                <div className='flex flex-row items-center justify-start   '>
+                  <div className='px-5'>
+                  <FormLabel >projet:</FormLabel>
+                  </div>
+                  <FormControl>
+                  
+                    <Input className=' rounded-full w-300 h-7'
+                     
+                      placeholder=" "
+                      {...field}
+                      
+                    />
+                    
+                  </FormControl>
+                  </div>
+                  <FormMessage />
+                </FormItem>
+              </>
+            )}
+            />
+
+
+           <div className='p-5'>
+          <Button className='focus:outline font-medium rounded-lg text-sm p-5 py-2.5 mb-2 h-[35px]  bg-buttonDark  text-textLight hover:bg-slate-700 hover:text-textLight  ' type="submit">Ajouter</Button>
+          </div>
+           </form>
       </Form>
       <DevTool control={form.control} />
     </>
--- Frontend/src/Pages/Technical Pages/Control Pannel/Pages/AddProject.jsx
+++ Frontend/src/Pages/Technical Pages/Control Pannel/Pages/AddProject.jsx
@@ -1,6 +1,261 @@
 import React, { useState } from 'react';
 import { NavLink, Outlet } from 'react-router-dom';
+import { DevTool } from '@hookform/devtools';
+import { useForm } from 'react-hook-form';
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+ 
+export default function AddProject() {
+  const schema = yup.object().shape({
+    numero: yup.string().required("le numero est requis"),
+    titre: yup.string().required("le titre est requis"),
+    dateDebut: yup.string().required("la date de debut est requise"),
+    chefProjet: yup.string().required(" le chef de projet est requis  "),
+    dateFin : yup.string(),
+    theme : yup.string().required(" le theme est requis "),
+    listeMembres : yup.string().required(" la liste des membres est requise "),
+  });
 
-export default function AddProject(props) {
-  return <></>;
+  const form = useForm({
+    defaultValues: {
+      numero:"" ,
+    titre:"" ,
+    dateDebut: "",
+    chefProjet: "",
+    dateFin : "",
+    theme : "",
+    listeMembres : "",
+
+    },
+    resolver: yupResolver(schema),
+
+  });
+
+  const onSubmit = (data) => {
+    console.log('Filtres : ', data);
+  };
+ 
+  return <>
+  <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <div className='p-5 text-center '>
+            <h1 className='mb-4 text-xl font-bold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-2xl dark:text-white'>Ajouter un nouveau projet </h1>
+            </div>
+            <h2 className='text-1xl font-bold dark:text-white text-center'>veuillez remplir ce formulaire </h2>
+            <hr className='h-px my-8 bg-black  bg-opacity-50 border-0 '></hr>
+          <FormField
+            control={form.control}
+            name="numero"
+            render={({ field }) => (
+              <>
+              
+                <FormItem>
+                <div className='flex flex-row items-center justify-start  pt-5'>
+                  <div className='pl-5 pr-9 '>
+                  <FormLabel>Numero :</FormLabel>
+                  </div>
+                  <FormControl>
+                  
+                    <Input className=' rounded-full w-300 h-7'
+                     
+                      placeholder="entrez le numero "
+                      {...field}
+                      
+                      
+                    />
+                    
+                  </FormControl>
+                  
+                  </div>
+                  <FormMessage />
+                </FormItem> 
+              </>
+            )}
+          />
+          
+      <FormField
+            control={form.control}
+            name="titre"
+            render={({ field }) => (
+
+              <> 
+              <FormItem>
+                <div className='flex flex-row items-center justify-start   '>
+                  <div className='px-5'>
+                  <FormLabel >titre :</FormLabel>
+                  </div>
+                  <FormControl>
+                  
+                    <Input className=' rounded-full w-300 h-7'
+                     
+                      placeholder="entrez le titre "
+                      {...field}
+                      
+                    />
+                    
+                  </FormControl>
+                  </div>
+                  <FormMessage />
+                </FormItem>
+              </>
+            )}
+            />
+            
+            <FormField
+            control={form.control}
+            name="dateDebut"
+            render={({ field }) => (
+              <>
+              
+                <FormItem>
+                <div className='flex flex-row items-center justify-start  '>
+                  <div className='pl-5 pr-9 '>
+                  <FormLabel>Date de debut :</FormLabel>
+                  </div>
+                  <FormControl>
+                  
+                    <Input className=' rounded-full w-300 h-7'
+                     
+                      placeholder="entrez la date de debut "
+                      {...field}
+                      
+                    />
+                    
+                  </FormControl>
+                  </div>
+                  <FormMessage />
+                </FormItem> 
+              </>
+            )}
+          />
+            <FormField
+            control={form.control}
+            name="chefProjet"
+            render={({ field }) => (
+
+              <> 
+              <FormItem>
+                <div className='flex flex-row items-center justify-start   '>
+                  <div className='px-5'>
+                  <FormLabel >Chef de Projet :</FormLabel>
+                  </div>
+                  <FormControl>
+                  
+                    <Input className=' rounded-full w-300 h-7'
+                     
+                      placeholder=" entrez le chef de Projet"
+                      {...field}
+                      
+                    />
+                    
+                  </FormControl>
+                  </div>
+                  <FormMessage />
+                </FormItem>
+              </>
+            )}
+            />
+            
+            
+            <FormField
+            control={form.control}
+            name="dateFin"
+            render={({ field }) => (
+
+              <> 
+              <FormItem>
+                <div className='flex flex-row items-center justify-start   '>
+                  <div className='px-5'>
+                  <FormLabel >date de fin :</FormLabel>
+                  </div>
+                  <FormControl>
+                  
+                    <Input className=' rounded-full w-300 h-7'
+                     
+                      placeholder=" entrez la date de fin "
+                      {...field}
+                      
+                    />
+                    
+                  </FormControl>
+                  </div>
+                  <FormMessage />
+                </FormItem>
+              </>
+            )}
+            />
+            <FormField
+            control={form.control}
+            name="theme"
+            render={({ field }) => (
+
+              <> 
+              <FormItem>
+                <div className='flex flex-row items-center justify-start   '>
+                  <div className='px-5'>
+                  <FormLabel >Theme :</FormLabel>
+                  </div>
+                  <FormControl>
+                  
+                    <Input className=' rounded-full w-300 h-7'
+                     
+                      placeholder=" entrez le theme"
+                      {...field}
+                      
+                    />
+                    
+                  </FormControl>
+                  </div>
+                  <FormMessage />
+                </FormItem>
+              </>
+            )}
+            />
+            <FormField
+            control={form.control}
+            name="listeMembres"
+            render={({ field }) => (
+
+              <> 
+              <FormItem>
+                <div className='flex flex-row items-center justify-start   '>
+                  <div className='px-5'>
+                  <FormLabel >liste des membres :</FormLabel>
+                  </div>
+                  <FormControl>
+                  
+                    <Input className=' rounded-full w-300 h-7'
+                     
+                      placeholder=" entrez la liste des membres "
+                      {...field}
+                      
+                    />
+                    
+                  </FormControl>
+                  </div>
+                  <FormMessage />
+                </FormItem>
+              </>
+            )}
+            />
+           <div className='p-5'>
+          <Button className='focus:outline font-medium rounded-lg text-sm p-5 py-2.5 mb-2 h-[35px]  bg-buttonDark  text-textLight hover:bg-slate-700 hover:text-textLight  ' type="submit">Ajouter</Button>
+          </div>
+           </form>
+      </Form>
+      <DevTool control={form.control} />
+  
+  
+  </>;
 }
