fdeb577e35e4207f130ac5ad615c467a8df52c12
rawane0310 <118125151+rawane0310@users.noreply.github.com>
2024-04-25 01:13:03 +0100

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ app.use(cookie());
 const { get_maj_time } = require('./settings/changeTimeOfMaj');
 const { maj } = require("./màj/majM");//pour la maj
 
-const { nouveau_projet } = require('./ROUTERS/crud_project/controllers')
+
 app.use(cors())
 app.use(express.json())
 
@@ -34,35 +34,21 @@ app.use('/api/v1/home', crud);
 
 const PORT = process.env.PORT || 3000
 
-app.listen(5173, () => {
-    console.log(`Serveur démarré sur le port `);
-});
 
-/*mongoose.connect(process.env.URL).then(() => {
+
+mongoose.connect(process.env.URL).then(() => {
     app.listen(PORT, async () => {
         console.log("connected to the database and start listening at post 3000..")
         let maj_time = await get_maj_time();
 
         if (maj_time !== -1) {
-            maj_time = maj_time[0] * 12 + maj_time[1];*/
-           // cron.schedule(`* * * * */${maj_time}`, maj)
+            maj_time = maj_time[0] * 12 + maj_time[1];
+           cron.schedule(`* * * * */${maj_time}`, maj)
 
-    /*    }
+      }
     })
 
 }).catch((err) => {
     console.log("can't connect ", err)
-})*/
+})
 
-const dbURI = "mongodb+srv://jazmine:11112024@cluster0.okfd2cg.mongodb.net/?retryWrites=true&w=majority";
-mongoose.connect(dbURI).then((result) => console.log("connected")).catch((err) => console.error(err));
-
-app.post('/insertProjet', (req,res) => {
-    // console.log(req.body);
-    nouveau_projet(req,res).then( resultat => {
-     console.log(resultat);
-    }).catch(err => {
-     console.error(err);
-    });
-     
- });
