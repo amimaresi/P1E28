81ea8b3ace2f1f14200c06040d387090b94294a5
rawane0310 <118125151+rawane0310@users.noreply.github.com>
2024-05-03 23:21:08 +0100

    Update ELayout.jsx

--- Frontend/src/Pages/Profiles/Encadrement/ELayout.jsx
+++ Frontend/src/Pages/Profiles/Encadrement/ELayout.jsx
@@ -44,29 +44,28 @@ export default function ELayout() {
   //   Equipe: 'Optimisation',
   // },
 });
-console.log("rryujh")
+
   const {id} = useParams()
 useEffect( ()=>{
-   console.log('ydrbeffect')
+
   const fetchtData = async()=>{
     try{
-      console.log("660096a07654baa3c3bed13a")
-       
-        const result = await axios.get(`http://localhost:3000/encadrements/encadrement/660096a07654baa3c3bed13a`)
-      console.log("rrrrrr")
+   
+        const result = await axios.get(`http://localhost:3000/encadrements/encadrement/${id}`)
+    
       console.log(result.data.Encadrements )
       setData(result.data.Encadrements)
       return result
       
     }catch(err){
-      console.log("errortttttttttttttttttttttttttttttttttttt")
+      console.log("error")
       console.log(err)
     }
   }
   fetchtData()
   
   } , [])
-  console.log('krlbbbbbbbbbbbbbbbbbb')
+ 
   console.log(data.Encadrements)
   return <>
    <div className='bg-white'>
