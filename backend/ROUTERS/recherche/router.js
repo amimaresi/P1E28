e19ba28b9a5e8017a5220aee9ce32b67d6d69398
rawane0310 <118125151+rawane0310@users.noreply.github.com>
2024-05-04 01:19:18 +0100

    Update router.js

--- backend/ROUTERS/recherche/router.js
+++ backend/ROUTERS/recherche/router.js
@@ -1,5 +1,6 @@
 const {queryChercheur , queryChercheurByid} = require('./rechercheChercheur')
 const {queryPublication , queryPublicationByid} = require('./recherchePublic')
+const {rechercheConference, rechercherConfParId} = require('./recherche_confJourn')
 const RechercheRouter = require('express').Router()
 
 
@@ -7,6 +8,8 @@ RechercheRouter.post('/Chercheur', queryChercheur)
 RechercheRouter.get('/Chercheur/:id', queryChercheurByid)
 RechercheRouter.post('/Publication', queryPublication)
 RechercheRouter.get('/Publication/:id', queryPublicationByid)
+RechercheRouter.post('/ConfJourn', rechercheConference )
+RechercheRouter.get('/ConfJourn/:id', rechercherConfParId )
 
 
 
