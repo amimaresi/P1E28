52fc90829fba8ebfbff42c99cffc6a41c7c47b30
rawane0310 <118125151+rawane0310@users.noreply.github.com>
2024-05-04 01:29:48 +0100

    Update routeConf.js

--- backend/ROUTERS/crudConfJourn/routeConf.js
+++ backend/ROUTERS/crudConfJourn/routeConf.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const router = express.Router;
-const {rechercheConference, rechercherConfParId} = require("../../recherche/recherche_confJourn");
+
 const ajouterModifierPeriode = require("./AjouterModifierPeriod");
 
-router.get('/confJourn?',rechercheConference); //recherche
-router.get('/confJourn/:id', rechercherConfParId); //recherche par id
+
 router.post('/confJourn/ajouterPeriode', ajouterModifierPeriode); //ajouter modifier periodicité
 module.exports = router:
