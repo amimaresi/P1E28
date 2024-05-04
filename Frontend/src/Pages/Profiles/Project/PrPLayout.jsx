5bac1a33330020f2bb53fd3cb858162e6fa5f96e
rawane0310 <118125151+rawane0310@users.noreply.github.com>
2024-05-04 02:11:28 +0100

    Update PrPLayout.jsx

--- Frontend/src/Pages/Profiles/Project/PrPLayout.jsx
+++ Frontend/src/Pages/Profiles/Project/PrPLayout.jsx
@@ -25,7 +25,7 @@ useEffect( ()=>{
    
   const fetchtData = async()=>{
     try{
-      const result = await axios.get(`http://localhost:3000/insertions/projet/search/${id}`)
+      const result = await axios.get(`http://localhost:3000/recherche/projet/${id}`)
       
       
       console.log(result.data.projet)
