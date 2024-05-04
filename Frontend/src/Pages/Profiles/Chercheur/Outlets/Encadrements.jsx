4b082f0d06a58a7ef3f4ce00d577c6c56c10b84a
SalahEddineMds <mm_mendas@esi.dz>
2024-05-04 11:26:05 +0100

    Profile Chercheur Done.

--- Frontend/src/Pages/Profiles/Chercheur/Outlets/Encadrements.jsx
+++ Frontend/src/Pages/Profiles/Chercheur/Outlets/Encadrements.jsx
@@ -1,10 +1,83 @@
 import React, { useState } from 'react';
-import { NavLink, Outlet } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 export default function Encadrements() {
-  return(
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [data, setData] = useState([
+    {
+      "_id": {
+        "$oid": "660096a07654baa3c3bed13a"
+      },
+      "Type": "PFE",
+      "Titre": "Base de donnée",
+      "AnneeD": "2020",
+      "AnneeF": "2021",
+      "Etudiants": [
+        "Ait kaci Azzou Sarah",
+        "Bachferrag Bouchra"
+      ],
+      "Encadrants": [
+        {
+          "NomComplet": "Amar Balla",
+          "_id": "a_balla@esi.dz",
+          "role": "encadrant"
+        },
+        {
+          "NomComplet": "Samira Azzou",
+          "_id": "Null",
+          "role": "Null"
+        }
+      ]
+    },
+    {
+      "_id": {
+        "$oid": "660096a07654baa3c3bed13b"
+      },
+      "Type": "PFE",
+      "Titre": "Intelligence Artificielle",
+      "AnneeD": "2020",
+      "AnneeF": "2021",
+      "Etudiants": [
+        "Heti Amina",
+        "Soupe Baraka"
+      ],
+      "Encadrants": [
+        {
+          "nomComplet": "Boualem Khelouat",
+          "_id": "b_khelouat@esi.dz",
+          "role": "encadrant"
+        },
+        {
+          "nomComplet": "Fatima Si Tayeb ",
+          "_id": "f_sitayeb@esi.dz",
+          "role": "co-encadrant"
+        }
+      ]
+    },
+    // Les autres éléments de données...
+  ]);
+
+  return (
     <>
-      <div>Encadrements</div>
+      <div className="bg-white p-8">
+        <ul>
+          <li className="mb-4 px-4 py-2 flex justify-between items-center">
+            <span className="text-lg font-bold">Titre</span>
+            <span className="text-lg font-bold">Type</span>
+            <span className="text-lg font-bold">Année Début</span>
+          </li>
+        </ul>
+
+        <ul>
+          {data.map((item, index) => (
+            <li className="mb-4 border rounded-lg bg-gray-100 px-4 py-2 flex justify-between items-center" key={index}>
+              <NavLink to={`/encadrement/{_id}`} className="text-sm hover:text-buttonDark">{item.Titre}</NavLink>
+              <span>{item.Type}</span>
+              <span>{item.AnneeD}</span>
+            </li>
+          ))}
+        </ul>
+      </div>      
     </>
-  )
+  );
 }
--- Frontend/src/Pages/Profiles/Chercheur/Outlets/Publications.jsx
+++ Frontend/src/Pages/Profiles/Chercheur/Outlets/Publications.jsx
@@ -71,7 +71,7 @@ export default function Publications() {
       <ul>
         {data.map((item, index) => (
           <li className="mb-4 border rounded-lg bg-gray-100 px-4 py-2 flex justify-between items-center" key={index}>
-            <a href={item.Lien} className="text-xs" target="_blank" rel="noopener noreferrer">{item.Titre}</a>
+            <a href={item.Lien} className="text-xs hover:text-buttonDark" target="_blank" rel="noopener noreferrer">{item.Titre}</a>
             <span>{item.rang}</span>
           </li>
         ))}
