c2451c51a42f4eb2668628f532fcfeb9f37e94de
rawane0310 <118125151+rawane0310@users.noreply.github.com>
2024-05-08 23:06:16 +0100

    Create InfoPageAccueil.js

---
+++ backend/schema/InfoPageAccueil.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const InfoPageAcceuilSchema = new Schema ({
+_id: {     //acronyme
+    type: String,
+    required: true
+},
+
+title: { //journal/conf..
+    type: String,
+    required: true
+},
+paragraphe: {
+    type: String,
+   // required: true
+},
+img: {
+    type: String,
+    //required: true
+},
+Subject: {
+    type: String,
+    //required: true
+}
+}, { timestamps: true });
+
+const InfoPageAcceuil = mongoose.model("InfoPageAcc",InfoPageAcceuilSchema);
+module.exports = InfoPageAcceuil;
