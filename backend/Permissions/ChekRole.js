c44536c2df1200f27ea3c1e31777112fe13f34f3
rawane0310 <118125151+rawane0310@users.noreply.github.com>
2024-05-08 19:14:15 +0100

    Create ChekRole.js

---
+++ backend/Permissions/ChekRole.js
@@ -0,0 +1,16 @@
+function verifierRole(rolesAutorises) {
+    return function(req, res, next) {
+        const roleUtilisateur = req.headers.role; // Supposons que le rôle de l'utilisateur est envoyé dans les en-têtes de la demande
+
+        // Vérifier si le rôle de l'utilisateur est autorisé à accéder à la fonctionnalité
+        if (rolesAutorises.includes(roleUtilisateur)) {
+            // L'utilisateur a le bon rôle, accorder l'accès
+            next();
+        } else {
+            // L'utilisateur n'a pas le bon rôle, renvoyer une erreur d'accès interdit
+            return res.status(403).json({ error: 'Accès interdit' });
+        }
+    };
+}
+
+module.exports = verifierRole
