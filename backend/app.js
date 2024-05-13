58278ff075843a8fcd84c444a864dfd09645e03b
rawane0310 <118125151+rawane0310@users.noreply.github.com>
2024-05-14 00:40:12 +0100

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -22,7 +22,7 @@ app.use(express.json())
 
 
 //app.use(cors())
-app.use(cors( {origin:'http://localhost:5173', credentials: true }))
+app.use(cors( {origin:'http://localhost:5174', credentials: true }))
 
 
 const routerConf = require('./ROUTERS/crudConfJourn/routeConf')
