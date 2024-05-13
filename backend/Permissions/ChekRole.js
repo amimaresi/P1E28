50ea057c1a460e6526a4b0cfd5c9d52939858ae6
rawane0310 <118125151+rawane0310@users.noreply.github.com>
2024-05-13 03:20:08 +0100

    Update ChekRole.js

--- backend/Permissions/ChekRole.js
+++ backend/Permissions/ChekRole.js
@@ -1,8 +1,20 @@
+const jwt = require('jsonwebtoken');
 function verifierRole(rolesAutorises) {
     return function(req, res, next) {
-        const roleUtilisateur = req.headers.type; // Supposons que le rôle de l'utilisateur est envoyé dans les en-têtes de la demande
-
-        // Vérifier si le rôle de l'utilisateur est autorisé à accéder à la fonctionnalité
+        //const roleUtilisateur = req.headers.type; // Supposons que le rôle de l'utilisateur est envoyé dans les en-têtes de la demande
+        const token = req.params;
+        console.log(token )
+         // Vérifier si le token existe
+         if (!token) {
+            return res.status(401).json({ error: 'Token JWT manquant' });
+        }
+        try {
+            const decodedToken = jwt.verify(token, process.env.SECRET_KEY);
+             console.log(decodedToken)
+        // Récupérer le rôle de l'utilisateur à partir des informations du token
+            const roleUtilisateur = decodedToken.type;
+        
+            // Vérifier si le rôle de l'utilisateur est autorisé à accéder à la fonctionnalité
         if (rolesAutorises.includes(roleUtilisateur)) {
             // L'utilisateur a le bon rôle, accorder l'accès
             next();
@@ -10,6 +22,10 @@ function verifierRole(rolesAutorises) {
             // L'utilisateur n'a pas le bon rôle, renvoyer une erreur d'accès interdit
             return res.status(403).json({ error: 'Accès interdit' });
         }
+    }  catch (error) {
+        // Une erreur s'est produite lors de la vérification ou du décodage du token JWT
+        return res.status(401).json({ error: 'Token JWT invalide' });
+    }
     };
 }
 
