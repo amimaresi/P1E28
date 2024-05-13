fd64938de890e7150cf36e4cb2b3d928a10f055a
beskie04 <164942527+beskie04@users.noreply.github.com>
2024-05-13 13:31:50 +0100

    Update ELayout.jsx

--- Frontend/src/Pages/Profiles/Encadrement/ELayout.jsx
+++ Frontend/src/Pages/Profiles/Encadrement/ELayout.jsx
@@ -67,66 +67,79 @@ export default function ELayout() {
   console.log(data.Encadrements);
   return data ? (
     <>
-      <div className="bg-white">
-        <div className="p-12">
-          <div className=" flex justify-center ">
-            {/* titre de l'Encadrement*/}
-            <h1 className="    mb-4 px-2 pb-6 pt-2 text-xl font-bold leading-none tracking-tight text-gray-900 dark:text-white md:text-5xl lg:text-2xl">
-              Encadrement:
-            </h1>
-            <h1 className="    mb-4 px-9 pb-6 pt-2 text-xl font-bold leading-none tracking-tight text-gray-900 dark:text-white md:text-5xl lg:text-2xl">
-              {' '}
-              {data.Titre}
-            </h1>
-          </div>
+       <div className='bg-white'>
+   <div className='p-12'>
+   
+   <div className="  bg-buttonDark flex items-center justify-left rounded-2xl h-48">
+  {/* titre de l'Encadrement*/}
+ 
+  <h1 className='    pt-2 pb-6 px-9 mb-4 text-xl font-bold leading-none tracking-tight text-white md:text-5xl lg:text-2xl dark:text-white'> {data.Titre}</h1>
+  </div>
 
-          <div className="flex justify-center space-x-20 p-4 ">
-            <div>
-              {/* type de l'Encadrement*/}
-              <h3 className="pb-2 font-bold">Type</h3>
-              <h3>{data.Type}</h3>
-            </div>
 
-            <div>
-              {/* anneeD de l'Encadrement*/}
-              <h3 className="pb-2 font-bold">année debut</h3>
-              <h3>{data.AnneeD}</h3>
-            </div>
+  <div className='p-4 flex space-x-20 justify-center '>
 
-            <div>
-              {/* anneeF de l'Encadrement*/}
-              <h3 className="pb-2 font-bold">année fin</h3>
-              <h3>{data.AnneeF}</h3>
-            </div>
-          </div>
 
-          <div className=" px-64 ">
-            {/* La liste des etudiants de l'Encadrement*/}
-            <h2 className="p-6 font-bold">La liste des étudiants:</h2>
-            {data.Etudiants &&
-              data.Etudiants.map((Etud) => (
-                <div className="   mb-4 mr-4 flex h-16 w-full items-center rounded-2xl border  bg-[#EFF3FF]  p-4   ">
-                  <h3>_{Etud}</h3>
-                </div>
-              ))}
-          </div>
 
-          <div className=" px-64 ">
-            {/* La liste des encadrants de l'Encadrement*/}
-            <h2 className="p-6 font-bold">La liste des encadrants:</h2>
-            {data.Encadrants &&
-              data.Encadrants.map((Encad, index) => (
-                <div className="   mb-4 mr-4 flex h-16 w-full items-center rounded-2xl border  bg-[#EFF3FF]  p-4   ">
-                  <h3 className="px-2">
-                    {index + 1}_{Encad.NomComplet}{' '}
-                  </h3>
-                  <h3 className="px-2 font-bold"> le role : </h3>
-                  <h3>{Encad.role}</h3>
-                </div>
-              ))}
-          </div>
-        </div>
-      </div>
+  <div className="flex justify-center rounded-xl border bg-buttonDark px-12 pb-4 pt-4 text-white items-center">
+      {/* type de l'Encadrement*/}
+      <h3 className='font-bold  pr-4'>Type :</h3>
+      <h3>{data.Type}</h3>
+    </div>
+
+    
+
+
+    <div className="flex justify-center rounded-xl border bg-buttonDark px-12 pb-4 pt-4 text-white items-center">
+      {/* anneeD de l'Encadrement*/}
+      <h3 className='font-bold  pr-4'>année debut:</h3>
+      <h3>{data.AnneeD}</h3>
+    </div>
+
+
+
+    <div className="flex justify-center rounded-xl border bg-buttonDark px-12 pb-4 pt-4 text-white items-center">
+      {/* anneeF de l'Encadrement*/}
+      <h3 className='font-bold  pr-4'>année fin:</h3>
+      <h3>{data.AnneeF}</h3>
+    </div>
+
+
+
+
+    </div>
+
+
+    <div className="  ">
+    {/* La liste des etudiants de l'Encadrement*/}
+  <h2 className='p-6 font-bold'>La liste des étudiants:</h2>
+    {data.Etudiants && data.Etudiants.map((Etud   )=>
+      <div  className="   flex border bg-[#EFF3FF] rounded-2xl p-4 mr-4 w-full h-16  mb-4      ">
+     <h3 >_{Etud}</h3>
+     
+     </div>
+      )}
+</div>
+
+
+
+<div className="  ">
+    {/* La liste des encadrants de l'Encadrement*/}
+  <h2 className='p-6 font-bold'>La liste des encadrants:</h2>
+    {data.Encadrants && data.Encadrants.map((Encad , index  )=>
+      <div  className="   flex border bg-[#EFF3FF] rounded-2xl p-4 mr-4 w-full h-16  mb-4  items-center   ">
+     <h3 className='px-2' >{index +1 }_{Encad.NomComplet}  </h3>
+     <h3 className='px-2 font-bold' > le role : </h3>
+     <h3 >{Encad.role}</h3>
+     </div>
+      )}
+</div>
+    
+
+
+
+    </div>
+   </div>
     </>
   ) : (
     <NotFound />
