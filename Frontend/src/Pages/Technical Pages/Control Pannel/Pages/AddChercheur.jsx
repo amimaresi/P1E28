f748dad221910c68600dc33fd638adecf5d922dd
beskie04 <164942527+beskie04@users.noreply.github.com>
2024-05-14 18:21:49 +0100

    Update AddChercheur.jsx

--- Frontend/src/Pages/Technical Pages/Control Pannel/Pages/AddChercheur.jsx
+++ Frontend/src/Pages/Technical Pages/Control Pannel/Pages/AddChercheur.jsx
@@ -52,7 +52,10 @@ export default function AddChercheur() {
     lien: yup.string(),
     projet: yup.string(),
   });
-
+const [isSubmitted, setIsSubmitted] = useState(false);
+  const handleButtonClick = () => {
+    form.handleSubmit(onSubmit)();
+  };
   const form = useForm({
     defaultValues: {},
     resolver: yupResolver(schema),
@@ -77,6 +80,7 @@ export default function AddChercheur() {
 
       console.log(info);
       console.log('clicked');
+      setIsSubmitted(true);
 
       const res = await axios.post(
         'http://localhost:3000/chercheur/insertionChercheur ',
@@ -479,9 +483,13 @@ export default function AddChercheur() {
                 />
 
                 <div className="flex gap-3 p-5">
+                  <div className="text-green-600 font-bold text-center">
+        {isSubmitted && <p> ajouté avec succès</p>}
+      </div>
                   <Button
                     className="mb-2 h-[35px] rounded-lg bg-buttonDark p-5 py-2.5 text-sm font-medium  text-textLight  hover:bg-slate-700 hover:text-textLight focus:outline  "
                     type="submit"
+                    onClick={handleButtonClick}
                   >
                     Ajouter
                   </Button>
